f86ead5bb74bd471c4bcd0107b48f81814f17791
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-21 17:22:17 -0300

    71 - Grid de Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -35,7 +35,21 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
-    return Container();
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: snapshot.data['data'].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data['data'][index]['images']['fixed_height']['url'],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -78,8 +92,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                   );
                 default:
-                  if (snapshot.hasError) return Container();
-                  else _createGifTable(context, snapshot);
+                  if (snapshot.hasError)
+                    return Container();
+                  else
+                    return _createGifTable(context, snapshot);
               }
             },
           ))
