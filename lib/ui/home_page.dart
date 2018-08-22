d4fe7b748f508a49a9a7605e62e20bf79a63586f
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-21 22:04:56 -0300

    72 - Carregando mais Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -20,9 +20,16 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=SqUOijUASNaDiCiLiE0pcRqJIZlrZ5TA&limit=20&rating=G");
     else
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=SqUOijUASNaDiCiLiE0pcRqJIZlrZ5TA&q=$_search&limit=20&offset=$_offset&rating=G&lang=pt");
+          "https://api.giphy.com/v1/gifs/search?api_key=SqUOijUASNaDiCiLiE0pcRqJIZlrZ5TA&q=$_search&limit=19&offset=$_offset&rating=G&lang=pt");
 
     return json.decode(response.body);
+  }
+
+  int _getCount(List data) {
+    if (_search == null)
+      return data.length;
+    else
+      return data.length + 1;
   }
 
   @override
@@ -39,15 +46,37 @@ class _HomePageState extends State<HomePage> {
       padding: EdgeInsets.all(10.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
-      itemCount: snapshot.data['data'].length,
+      itemCount: _getCount(snapshot.data['data']),
       itemBuilder: (context, index) {
-        return GestureDetector(
-          child: Image.network(
-            snapshot.data['data'][index]['images']['fixed_height']['url'],
-            height: 300.0,
-            fit: BoxFit.cover,
-          ),
-        );
+        if (_search == null || index < snapshot.data['data'].length) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        } else {
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 70.0),
+                  Text(
+                    'Carregar mais...',
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  )
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  _offset += 19;
+                });
+              },
+            ),
+          );
+        }
       },
     );
   }
@@ -73,6 +102,12 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
