29e50b65b7871fdadfe1159877c314b9db971df4
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-21 23:19:10 -0300

    74 - Compartilhando Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 import 'gif_page.dart';
 
@@ -63,6 +64,9 @@ class _HomePageState extends State<HomePage> {
                   MaterialPageRoute(
                       builder: (context) =>
                           GifPage(snapshot.data['data'][index])));
+            },
+            onLongPress: () {
+              Share.share(snapshot.data['data'][index]['images']['fixed_height']['url']);
             },
           );
         } else {
